00d93272838f4871b3a662b8b2bda26df8bd2100
JJJbc <qudcjf854@gmail.com>
2025-05-29 16:58:04 +0900

    spring 추가

---
+++ woori-app1/src/main/java/com/woori/woori_app/WooriAppApplication.java
@@ -0,0 +1,13 @@
+package com.woori.woori_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooriAppApplication.class, args);
+	}
+
+}
